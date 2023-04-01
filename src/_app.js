d414603e8bb3f1181f79d9b9a57e66a6975991f5
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-04-01 12:33:44 +0100

    ReactFacts
    ReactFacts

---
+++ src/_app.js
@@ -0,0 +1,28 @@
+import '@/styles/globals.css'
+import Navbar from "@/components/Navbar"
+import Main from "@/components/Main"
+
+export default function App() {
+  const [darkMode, setDarkMode] = React.useState(true)
+    
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+    
+    return (
+        <div className="container">
+            <Navbar 
+                darkMode={darkMode} 
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main darkMode={darkMode} />
+        </div>
+    )
+}
+
+
+
+
+export default function App() {
+    
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import App from '@/_app.js';
+
+ReactDOM.render(<App />, document.getElementById("root"))
